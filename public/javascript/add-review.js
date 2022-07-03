f8cf7cb74d346b323070b866e5403f6a14e4768f
Katya Juliet Lerner <katyajuliet22@gmail.com>
2022-07-03 14:32:52 -0700

    fixing error from add-review.js

--- public/javascript/add-review.js
+++ public/javascript/add-review.js
@@ -3,15 +3,8 @@ async function newFormHandler(event) {
 
   const title = document.querySelector('input[name="review-title"]').value;
   const review_url = document.querySelector('input[name="review-url"]').value;
-<<<<<<< HEAD
-  const review_file =  document.querySelector('input[name="review-file"]').value;
-  const review_cat =  document.getElementById('review_cat').value;
-
-
-=======
   const review_file = document.querySelector('input[name="review-file"]').value;
-  const review_cat = document.querySelector('select[name="review-cat"]').value;
->>>>>>> 57dd820fe1e5caa8f17989131194916eb5771535
+  const review_cat = document.querySelector('select[name="review_cat"]').value;
 
   const response = await fetch(`/api/reviews`, {
     method: "POST",
@@ -33,6 +26,5 @@ async function newFormHandler(event) {
   }
 }
 
-document
-  .querySelector(".new-review-form")
-  .addEventListener("submit", newFormHandler);
+document.querySelector(".new-review-form").addEventListener("submit", newFormHandler);
+  
